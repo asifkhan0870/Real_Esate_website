8404b9fe9bcd7785e1d6ec1c228f9ed9ad72aa8b
Asif khan <asifkhan@Asifs-MacBook-Air.local>
2024-04-23 08:21:51 +0530

    making responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,42 @@
-import React from "react";
-import './Header.css';
+import React, { useState } from "react";
+import "./Header.css";
+import { IoMdMenu } from "react-icons/io";
 
+import OutsideClickHandler from "react-outside-click-handler";
 
 const Header = () => {
+  const [menuOpen, setmenuOpen] = useState(false);
+
+  const getMenuStyle = (menuOpen) => {
+    if (document.documentElement.clientWidth <= 770) {
+      return { right: !menuOpen && "-100%" };
+    }
+  };
+
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="" />
 
-        <div className="flexCenter h-menu">
-          <a href="">Residencies</a>
-          <a href="">Our Value</a>
-          <a href="">Contact Us</a>
-          <a href="">Get started</a>
-          <button className="button">
-          <a href="">Contact</a>
-          </button>
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setmenuOpen(false);
+          }}
+        >
+          <div className="flexCenter h-menu" style={getMenuStyle(menuOpen)}>
+            <a href="">Residencies</a>
+            <a href="">Our Value</a>
+            <a href="">Contact Us</a>
+            <a href="">Get started</a>
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
+
+        <div className="menu-icon" onClick={() => setmenuOpen((prev) => !prev)}>
+          <IoMdMenu size={25} />
         </div>
-
-
       </div>
     </section>
   );
